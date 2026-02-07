e1bd801ed55f470eb24ce3684576fb91b14bfbd6
dheeraj7866 <93239707+dheeraj7866@users.noreply.github.com>
2026-02-07 13:23:10 +0530

    ccx

---
+++ app/app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Core SaaS API Running!"
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
